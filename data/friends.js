35d6d26460146931eda06d177559c94e964917c5
Marcus Johnson <597392@park.edu>
2018-05-20 14:24:34 -0500

    app

---
+++ data/friends.js
@@ -0,0 +1,22 @@
+// DATA
+// Below data will hold all of the friends.
+var friendsArray = 
+
+{
+    "name":"Ahmed",
+    "photo":"https://usafricanimmigrantsgist.com/wp-content/uploads/2018/03/abiy-ahmed.jpg",
+    "scores":[
+        3,
+        5,
+        2,
+        5,
+        5,
+        1,
+        3,
+        5,
+        4,
+        2
+      
+    ],
+}
+  
---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+// npm packages used to give our server useful functionality
+var express = require('express');
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// set up properties for express server
+// indicate to Node that we are creating express server
+var app = express();
+
+// Set a port that will be our listener later on
+var PORT = process.env.PORT || 8080;
+
+// set the express app to handle data parsing
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// ROUTER
+// this gives our server a map of how to respond when users visit or resquest data from various URLs.
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// LISTENER
+// this code effectively "starts" our server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
